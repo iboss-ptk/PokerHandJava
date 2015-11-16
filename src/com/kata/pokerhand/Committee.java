344a7bb383f07b6e93c5d0d047014e57d1147c20
Supanat <paul_iori@hotmail.com>
2015-11-16 16:50:17 +0700

    implement committee

---
+++ src/com/kata/pokerhand/Committee.java
@@ -0,0 +1,7 @@
+package com.kata.pokerhand;
+
+/**
+ * Created by Apple on 11/16/15 AD.
+ */
+public class Committee {
+}
---
+++ src/com/kata/pokerhand/CommitteeTest.java
@@ -0,0 +1,10 @@
+package com.kata.pokerhand;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Apple on 11/16/15 AD.
+ */
+public class CommitteeTest {
+
+}
